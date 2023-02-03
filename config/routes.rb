5f70d48caaac302963e9b4173595c282a5a6db6b
TheLazyCode-hub <gauravshukla052@gmail.com>
2023-02-03 12:12:06 +0530

    course module moved in admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,13 @@
 Rails.application.routes.draw do
   # resources :projects
-  resources :courses
+  # resources :courses
   resources :blogs
   resources :students
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :admin do
     resources :students
     resources :projects
+    resources :courses
     get 'dashboard' => 'dashboard#index'
   end
 end
