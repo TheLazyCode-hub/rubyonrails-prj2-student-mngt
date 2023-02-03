6e84a24ea77ea3c8f2e04226b00d2c905570cfcd
TheLazyCode-hub <gauravshukla052@gmail.com>
2023-02-03 12:32:32 +0530

    blogs moved into admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 Rails.application.routes.draw do
   # resources :projects
   # resources :courses
-  resources :blogs
-  resources :students
+  # resources :blogs
+  # resources :students
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :admin do
     resources :students
     resources :projects
     resources :courses
+    resources :blogs
     get 'dashboard' => 'dashboard#index'
   end
 end
